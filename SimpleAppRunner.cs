0bb5bfe5757a1b9c66f4d0dde4d4bae969b5aee0
JiboGithub <ajidejibola@yahoo.com>
2021-08-08 21:21:19 +0100

    added searchphrase and css selectors in list mode

--- SimpleAppRunner.cs
+++ SimpleAppRunner.cs
@@ -1,5 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using System.Linq;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
 
@@ -16,17 +18,25 @@ namespace SeleniumProject
             
             //Go to github and search for my username 
             driver.Navigate().GoToUrl("https://github.com");
-            IWebElement searchInputGithub = driver.FindElement(By.CssSelector("[name = 'q']"));
-            searchInputGithub.SendKeys("jibogithub");
+
+            List<string> searchPhrase = new List<string>();
+            searchPhrase.AddRange(new List<string> { "jibogithub", "iphone" });
+
+            List<string> searchSelectors = new List<string>();
+            searchSelectors.AddRange(new List<string> { "[name = 'q']", "[name='field-keywords']" });
+
+
+            IWebElement searchInputGithub = driver.FindElement(By.CssSelector(searchSelectors[0]));
+            searchInputGithub.SendKeys(searchPhrase.FirstOrDefault());
             searchInputGithub.SendKeys(Keys.Enter);
 
             //Go to amazon and search for iphone
             driver.Navigate().GoToUrl("https://www.amazon.com/");
-            IWebElement searchInput = driver.FindElement(By.CssSelector("[name='field-keywords']"));
-            searchInput.SendKeys("iphone");
+            IWebElement searchInput = driver.FindElement(By.CssSelector(searchSelectors[1]));
+            searchInput.SendKeys(searchPhrase[1]);
             searchInput.SendKeys(Keys.Enter);
             
-            driver.Quit();
+            //driver.Quit();
         }
     }
 }
