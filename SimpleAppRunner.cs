8443f37ea32312c9044e568e46860a42183b61bb
JiboGithub <ajidejibola@yahoo.com>
2021-08-08 20:17:21 +0100

    added go to amazon and search for iphone

--- SimpleAppRunner.cs
+++ SimpleAppRunner.cs
@@ -13,12 +13,19 @@ namespace SeleniumProject
 
             IWebDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
+            
+            //Go to github and search for my username 
             driver.Navigate().GoToUrl("https://github.com");
+            IWebElement searchInputGithub = driver.FindElement(By.CssSelector("[name = 'q']"));
+            searchInputGithub.SendKeys("jibogithub");
+            searchInputGithub.SendKeys(Keys.Enter);
 
-            IWebElement searchInput = driver.FindElement(By.CssSelector("[name = 'q']"));
-            searchInput.SendKeys("jibogithub");
+            //Go to amazon and search for iphone
+            driver.Navigate().GoToUrl("https://www.amazon.com/");
+            IWebElement searchInput = driver.FindElement(By.CssSelector("[name='field-keywords']"));
+            searchInput.SendKeys("iphone");
             searchInput.SendKeys(Keys.Enter);
-
+            
             driver.Quit();
         }
     }
