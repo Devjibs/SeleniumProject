6f12aa377f293ded4300127b3fdbba418614da01
JiboGithub <ajidejibola@yahoo.com>
2021-08-08 19:55:40 +0100

    Add project files.

---
+++ SimpleAppRunner.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumProject
+{
+    public class SimpleAppRunner
+    {
+        public static void Main(string[] args)
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://github.com");
+
+            IWebElement searchInput = driver.FindElement(By.CssSelector("[name = 'q']"));
+            searchInput.SendKeys("jibogithub");
+            searchInput.SendKeys(Keys.Enter);
+
+            driver.Quit();
+        }
+    }
+}
